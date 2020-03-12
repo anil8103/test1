4674c548a0995ffb39926c8ee33f09ea7296fe88
Kenchappa Mundiganal (Industrial KE40000196 Engineering Services (IKE40000196ES)) <KE40000196@wipro.com>
2020-03-12 12:57:43 +0530

    first commit

---
+++ SonarQubeAndroidAPP/src/main/java/com/example/sonarqubeandroidapp/SonarQubeAPPclass.java
@@ -0,0 +1,10 @@
+package com.example.sonarqubeandroidapp;
+
+public class SonarQubeAPPclass {
+    public boolean SampleTestmethod() {
+        return true;
+    }
+    public boolean NotSampleTestmethod() {
+        return true;
+    }
+}
---
+++ SonarQubeAndroidAPP/src/test/java/com/example/sonarqubeandroidapp/SonarQubeAPPclassTest.java
@@ -0,0 +1,10 @@
+package com.example.sonarqubeandroidapp;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SonarQubeAPPclassTest {
+    @Test public void testSampleTestmethod() {
+        SonarQubeAPPclass classUnderTest = new SonarQubeAPPclass();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.SampleTestmethod());
+    }
+}
